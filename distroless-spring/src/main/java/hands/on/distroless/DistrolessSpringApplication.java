737de4117fd88c5e11f22782fdb114863a635baf
Mario-Leander Reimer <mario-leander.reimer@qaware.de>
2020-05-27 17:28:09 +0200

    Added Distroless Spring boot demo.

---
+++ distroless-spring/src/main/java/hands/on/distroless/DistrolessSpringApplication.java
@@ -0,0 +1,13 @@
+package hands.on.distroless;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DistrolessSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DistrolessSpringApplication.class, args);
+	}
+
+}
---
+++ distroless-spring/src/test/java/hands/on/distroless/DistrolessSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package hands.on.distroless;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DistrolessSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
